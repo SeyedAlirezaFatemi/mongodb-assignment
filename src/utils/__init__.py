57114c49513a1bde53f142d88f27bcf5b84b9121
Alireza <alirezafa@rocketmail.com>
2021-01-02 23:57:38 +0330

    Added utils.

---
+++ src/utils/__init__.py
@@ -0,0 +1,13 @@
+import datetime
+
+
+def generate_national_id(fake, prev_national_ids):
+    nat_id = fake.numerify(text="##########")
+    while nat_id in prev_national_ids:
+        nat_id = fake.numerify(text="##########")
+    prev_national_ids.add(nat_id)
+    return nat_id
+
+
+def str_to_date(string):
+    return datetime.datetime.strptime(string, "%Y-%m-%d").date()
