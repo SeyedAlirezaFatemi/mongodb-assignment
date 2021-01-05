6780ff0128cfd40324ba9f4d3b1931de53d89f5b
Alireza <alirezafa@rocketmail.com>
2021-01-05 20:38:42 +0330

    :card_file_box: Improved store Models.

--- src/data/store_schema.py
+++ src/data/store_schema.py
@@ -26,10 +26,7 @@ SIZE = (
 
 
 class ProductItem(mongoengine.Document):
-    #     https://stackoverflow.com/questions/10144852/how-can-i-create-unique-ids-for-embedded-documents-in-mongodb
-    #     from bson.objectid import ObjectId
-    #     _id = ObjectIdField( required=True, default=lambda: ObjectId() )
-    product = mongoengine.ReferenceField(
+    product_id = mongoengine.ReferenceField(
         Product, reverse_delete_rule=mongoengine.CASCADE, unique_with=["size", "color"]
     )
     color = mongoengine.StringField(max_length=64, required=True)
@@ -38,29 +35,29 @@ class ProductItem(mongoengine.Document):
 
 
 class BasketItem(mongoengine.EmbeddedDocument):
-    product_item = mongoengine.ReferenceField(ProductItem, unique=True)
+    product_item_id = mongoengine.ReferenceField(ProductItem)
     quantity = mongoengine.IntField(min_value=1, default=1, required=True)
 
 
 class User(mongoengine.Document):
-    email = mongoengine.EmailField(required=True, primary_key=True)
+    email = mongoengine.EmailField(required=True, unique=True)
     first_name = mongoengine.StringField(max_length=64, required=True)
     last_name = mongoengine.StringField(max_length=64, required=True)
     city = mongoengine.StringField(max_length=64)
     address = mongoengine.StringField(max_length=256)
     telephone = mongoengine.StringField(max_length=32)
     password = mongoengine.StringField(max_length=256, required=True)
-    basket_items = mongoengine.EmbeddedDocumentListField(document_type=BasketItem)
+    basket = mongoengine.EmbeddedDocumentListField(document_type=BasketItem)
 
 
 class Driver(mongoengine.Document):
     national_id = mongoengine.StringField(
-        min_length=10, max_length=10, required=True, primary_key=True
+        min_length=10, max_length=10, required=True, unique=True
     )
     first_name = mongoengine.StringField(max_length=64, required=True)
     last_name = mongoengine.StringField(max_length=64, required=True)
     telephone = mongoengine.StringField(max_length=32)
-    plate_number = mongoengine.StringField(max_length=16)
+    license_plate = mongoengine.StringField(max_length=16)
 
 
 DELIVERY_TYPES = (
@@ -71,7 +68,7 @@ DELIVERY_STATUSES = (("P", "Pending"), ("R", "Registered"), ("C", "Completed"))
 
 
 class Order(mongoengine.Document):
-    user = mongoengine.ReferenceField(
+    user_id = mongoengine.ReferenceField(
         document_type=User, reverse_delete_rule=mongoengine.NULLIFY
     )
     date = mongoengine.DateField(required=True)
@@ -87,15 +84,17 @@ class Order(mongoengine.Document):
 class Delivery(mongoengine.Document):
     date = mongoengine.DateField(required=True)
     capacity = mongoengine.IntField(required=True)
-    driver = mongoengine.ReferenceField(Driver, reverse_delete_rule=mongoengine.NULLIFY)
+    driver_id = mongoengine.ReferenceField(
+        Driver, reverse_delete_rule=mongoengine.NULLIFY
+    )
     orders = mongoengine.ListField(mongoengine.ReferenceField(document_type=Order))
 
 
 class Comment(mongoengine.Document):
-    user = mongoengine.ReferenceField(
+    user_id = mongoengine.ReferenceField(
         document_type=User, reverse_delete_rule=mongoengine.NULLIFY
     )
-    product = mongoengine.ReferenceField(
+    product_id = mongoengine.ReferenceField(
         Product, reverse_delete_rule=mongoengine.CASCADE
     )
     rating = mongoengine.IntField(min_value=0, max_value=5, required=True)
