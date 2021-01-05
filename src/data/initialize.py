4d1253f165c61fb1ba3a9dc80518e3e0621a6ed1
Alireza <alirezafa@rocketmail.com>
2021-01-05 20:39:33 +0330

    :card_file_box: Improved initialize.

--- src/data/initialize.py
+++ src/data/initialize.py
@@ -3,7 +3,7 @@ import random
 from faker import Faker
 
 from src.utils import generate_national_id
-from .data import drug_formulas, drug_names, specialties
+from .data import drug_formulas, drug_names, product_names, specialties
 from .health_schema import (
     Company,
     Contract,
@@ -15,6 +15,16 @@ from .health_schema import (
     PrescriptionItem,
     Sale,
 )
+from .store_schema import (
+    BasketItem,
+    Comment,
+    Driver,
+    Product,
+    ProductItem,
+    SIZE,
+    TYPE,
+    User,
+)
 
 
 def initialize_db():
@@ -22,7 +32,9 @@ def initialize_db():
     Faker.seed(seed)
     random.seed(seed)
     fake = Faker()
-
+    #################
+    # Health System #
+    #################
     docs_national_ids = set()
     patients_national_ids = set()
     doctors = []
@@ -131,3 +143,82 @@ def initialize_db():
             }
         )
         prescription.save()
+    ################
+    # Store System #
+    ################
+    products = []
+    product_items = []
+    users = []
+    for _ in range(100):
+        seller = fake.company()
+        for i in range(random.randint(1, 3)):
+            product = Product(
+                **{
+                    "name": random.choice(product_names),
+                    "seller": seller,
+                    "ptype": TYPE[i][0],
+                    "price": random.uniform(20, 1000),
+                }
+            )
+            product.save()
+            products.append(product)
+
+    for product in products:
+        for i in range(random.randint(1, 4)):
+            product_item = ProductItem(
+                **{
+                    "product_id": product,
+                    "color": fake.color_name(),
+                    "size": SIZE[i][0],
+                    "quantity": random.randint(0, 500),
+                }
+            )
+            product_item.save()
+            product_items.append(product_item)
+
+    for _ in range(100):
+        basket = []
+        for product_item in random.sample(product_items, random.randint(1, 10)):
+            basket.append(
+                BasketItem(
+                    **{
+                        "product_item_id": product_item,
+                        "quantity": random.randint(1, 5),
+                    }
+                )
+            )
+        profile = fake.profile(["mail", "username", "address"])
+        user = User(
+            **{
+                "email": profile["mail"],
+                "first_name": fake.first_name(),
+                "last_name": fake.last_name(),
+                "address": profile["address"],
+                "password": profile["username"],
+                "city": fake.city(),
+                "telephone": fake.phone_number(),
+                "basket": basket,
+            }
+        )
+        user.save()
+        users.append(user)
+
+        driver = Driver(
+            **{
+                "national_id": generate_national_id(fake, patients_national_ids),
+                "first_name": fake.first_name(),
+                "last_name": fake.last_name(),
+                "telephone": fake.phone_number(),
+                "license_plate": fake.license_plate(),
+            }
+        )
+        driver.save()
+        comment = Comment(
+            **{
+                "user_id": random.choice(users),
+                "product_id": random.choice(products),
+                "text": fake.sentence(nb_words=10),
+                "rating": random.randint(0, 5),
+            }
+        )
+        comment.save()
